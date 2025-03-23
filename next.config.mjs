991588b9ef9e52e90ac571e341c7d35c7836aa53
Swastik2005 <swastikdevil123@gmail.com>
2025-03-24 04:36:32 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //  experimental: {
-  //   appDir: true,
-  //   serverComponentsExternalPackages: ["mongoose"],
-  //   staticPageGenerationTimeout: 0,
-  // },
-  // reactStrictMode: true,
-  // webpack(config) {
-  //   config.experiments = {
-  //     ...config.experiments,
-  //     topLevelAwait: true,
-  //   };
-  //   return config;
-  // },
+   experimental: {
+    appDir: true,
+    serverExternalPackages: ["mongoose"],
+    staticPageGenerationTimeout: 0,
+  },
+  reactStrictMode: true,
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+  cookies: {
+  sessionToken: {
+    name: `next-auth.session-token`,
+    options: {
+      httpOnly: true,
+      sameSite: 'lax',
+      path: '/',
+      secure: process.env.NODE_ENV === 'production'
+    }
+  }
+}
 };
 
 export default nextConfig;
